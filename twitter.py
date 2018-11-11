a5f440c753ec38530c2d797fcd97fa76fa0f45d7
flaviokosta <flaviokosta@gmail.com>
2018-11-11 20:53:53 -0300

    Aula 16 e 17

---
+++ twitter.py
@@ -0,0 +1,13 @@
+import oauth2
+
+class Twitter:
+
+    def __init__(self, consumer_key, consumer_secret, token_key, token_secret):
+        self.conexao(consumer_key, consumer_secret, token_key, token_secret)
+
+    def conexao(self, consumer_key, consumer_secret, token_key, token_secret):
+        self.consumer = oauth2.Consumer(consumer_key, consumer_secret)
+        self.token = oauth2.Token(token_key, token_secret)
+        self.cliente = oauth2.Client(self.consumer, self.token)
+
+    def tweet(self, novo_tweet):
